9e3e263284e243c670cb4acf0abae81016d28b58
Earl0935 <60341137+Earl0935@users.noreply.github.com>
2020-09-21 18:21:29 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,43 @@
+<!DOCtype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title> Home </title>
+</head>
+
+	<div id="Main">
+		<div class="header">
+		
+			<a href><div id="search">
+			<span id="search1"></span>
+			<span id="search2"></span>
+			</div></a>
+			
+			<div class="title">
+			
+			</div>
+			
+			<div class="logoboard">
+				<div id="logo">
+				<span id="logo1"></span>
+				<span id="logo2"></span>
+				
+				</div>
+			</div>
+			
+			
+	
+		</div>
+		<div class="content">
+		</div>
+		
+		
+		<footer> </footer>
+		
+		
+	</div>
+
+
+
+
+<body>
