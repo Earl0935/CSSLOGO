bb1defd79149efcaacd5b0e4b63d7beafe20c385
Earl0935 <60341137+Earl0935@users.noreply.github.com>
2020-10-03 22:58:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<!DOCtype html>
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <title> Home </title>
 </head>
+<body>
 
 	<div id="Main">
 		<div class="header">
@@ -13,6 +16,7 @@
 			</div></a>
 			
 			<div class="title">
+			
 			
 			</div>
 			
@@ -28,10 +32,38 @@
 	
 		</div>
 		<div class="content">
+			<div id="sidemenu">
+				<ul>
+					<li><a href>HOME</a></li>
+					<li><a href>ABOUT ME</a></li>
+					<li><a href>CONTACTS</a></li>
+				</ul>
+			</div>
+			<div id="main">
+				<h1>Wanna learn more about me?</h1>
+				<h1>This Site is all about me</h1>
+				<h1>Enjoy</h1>
+			</div>
 		</div>
 		
 		
-		<footer> </footer>
+		<footer> 
+			<ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+		</ul>
+			<i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+			<i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+			<i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+		</footer>
 		
 		
 	</div>
@@ -39,4 +71,6 @@
 
 
 
-<body>
+</body>
+</html>
+
